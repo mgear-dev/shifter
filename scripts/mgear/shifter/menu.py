c2ffeef4765d37755c9a5ead07508f066382b207
Ivo Grigull <ivgrigull@gmail.com>
2020-06-09 13:26:03 +0200

    Fix for issue "Menu: Ctrl+Shift results in broken shelf items"
    https://github.com/mgear-dev/shifter/issues/86

--- scripts/mgear/shifter/menu.py
+++ scripts/mgear/shifter/menu.py
@@ -1,39 +1,33 @@
-from functools import partial
 import mgear.menu
-from mgear import shifter
-from . import guide_template, mocap_tools, game_tools, io
-from . import guide_manager, guide_manager_gui, afg_tools_ui
 
 
 def install():
     """Install Shifter submenu
     """
     commands = (
-        ("Guide Manager", guide_manager_gui.show_guide_manager),
+        ("Guide Manager", str_show_guide_manager),
         ("-----", None),
-        ("Settings", partial(guide_manager.inspect_settings, 0)),
-        ("Duplicate", partial(guide_manager.duplicate, False)),
-        ("Duplicate Sym", partial(guide_manager.duplicate, True)),
-        ("Extract Controls", guide_manager.extract_controls),
+        ("Settings", str_inspect_settings),
+        ("Duplicate", str_duplicate),
+        ("Duplicate Sym", str_duplicateSym),
+        ("Extract Controls", str_extract_controls),
         ("-----", None),
-        ("Build from Selection", guide_manager.build_from_selection),
-        ("Build From Guide Template File", partial(io.build_from_file, None)),
+        ("Build from Selection", str_build_from_selection),
+        ("Build From Guide Template File", str_build_from_file),
         ("-----", None),
-        ("Import Guide Template", partial(io.import_guide_template, None)),
-        ("Export Guide Template", partial(io.export_guide_template,
-                                          None,
-                                          None)),
+        ("Import Guide Template", str_import_guide_template),
+        ("Export Guide Template", str_export_guide_template),
         ("-----", None),
         (None, guide_template_samples_submenu),
         ("-----", None),
-        ("Auto Fit Guide (BETA)", afg_tools_ui.show),
+        ("Auto Fit Guide (BETA)", str_auto_fit_guide),
         ("-----", None),
         (None, mocap_submenu),
-        ("Game Tools", game_tools.openGameTools),
+        ("Game Tools", str_openGameTools),
         ("-----", None),
-        ("Update Guide", guide_template.updateGuide),
+        ("Update Guide", str_updateGuide),
         ("-----", None),
-        ("Reload Components", shifter.reloadComponents)
+        ("Reload Components", str_reloadComponents)
     )
 
     mgear.menu.install("Shifter", commands)
@@ -46,9 +40,9 @@ def mocap_submenu(parent_menu_id):
         parent_menu_id (str): Parent menu. i.e: "MayaWindow|mGear|menuItem355"
     """
     commands = (
-        ("Import Mocap Skeleton Biped", mocap_tools.importSkeletonBiped),
-        ("Characterize Biped", mocap_tools.characterizeBiped),
-        ("Bake Mocap Biped", mocap_tools.bakeMocap)
+        ("Import Mocap Skeleton Biped", str_mocap_importSkeletonBiped),
+        ("Characterize Biped", str_mocap_characterizeBiped),
+        ("Bake Mocap Biped", str_mocap_bakeMocap)
     )
 
     mgear.menu.install("Mocap", commands, parent_menu_id)
@@ -61,10 +55,99 @@ def guide_template_samples_submenu(parent_menu_id):
         parent_menu_id (str): Parent menu. i.e: "MayaWindow|mGear|menuItem355"
     """
     commands = (
-        ("Biped Template",
-         partial(io.import_sample_template, "biped.sgt")),
-        ("Quadruped Template",
-         partial(io.import_sample_template, "quadruped.sgt"))
+        ("Biped Template", str_biped_template),
+        ("Quadruped Template", str_quadruped_template)
     )
 
     mgear.menu.install("Guide Template Samples", commands, parent_menu_id)
+
+
+str_show_guide_manager = """
+from mgear.shifter import guide_manager_gui
+guide_manager_gui.show_guide_manager()
+"""
+
+str_inspect_settings = """
+from mgear.shifter import guide_manager
+guide_manager.inspect_settings(0)
+"""
+
+str_duplicate = """
+from mgear.shifter import guide_manager
+guide_manager.duplicate(False)
+"""
+
+str_duplicateSym = """
+from mgear.shifter import guide_manager
+guide_manager.duplicate(True)
+"""
+
+str_extract_controls = """
+from mgear.shifter import guide_manager
+guide_manager.extract_controls()
+"""
+
+str_build_from_selection = """
+from mgear.shifter import guide_manager
+guide_manager.build_from_selection()
+"""
+
+str_build_from_file = """
+from mgear.shifter import io
+io.build_from_file(None)
+"""
+
+str_import_guide_template = """
+from mgear.shifter import io
+io.import_guide_template(None)
+"""
+
+str_export_guide_template = """
+from mgear.shifter import io
+io.export_guide_template(None, None)
+"""
+
+str_auto_fit_guide = """
+from mgear.shifter import afg_tools_ui
+afg_tools_ui.show()
+"""
+
+str_openGameTools = """
+from mgear.shifter import game_tools
+game_tools.openGameTools()
+"""
+
+str_updateGuide = """
+from mgear.shifter import guide_template
+guide_template.updateGuide()
+"""
+
+str_reloadComponents = """
+from mgear import shifter
+shifter.reloadComponents()
+"""
+
+str_biped_template = """
+from mgear.shifter import io
+io.import_sample_template("biped.sgt")
+"""
+
+str_quadruped_template = """
+from mgear.shifter import io
+io.import_sample_template("quadruped.sgt")
+"""
+
+str_mocap_importSkeletonBiped = """
+from mgear.shifter import mocap_tools
+mocap_tools.importSkeletonBiped()
+"""
+
+str_mocap_characterizeBiped = """
+from mgear.shifter import mocap_tools
+mocap_tools.characterizeBiped()
+"""
+
+str_mocap_bakeMocap = """
+from mgear.shifter import mocap_tools
+mocap_tools.bakeMocap()
+"""
